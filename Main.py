c2ff030101b17d431579e0e9dc346f4a9f888bd6
Shedit <pyrt.91@gmail.com>
2019-05-23 13:08:45 +0200

    Some wired changes with forgetting to save

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
-import numpy as np 
+#%% 
+#import numpy as np 
 import pandas as pd 
 import seaborn as sns 
+import matplotlib as plt
+file = pd.read_csv('Mall_Customers.csv')
+#%% 
 
+file.plot(kind = 'density')
+#%% 
 
-numpy
